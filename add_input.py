5a7ce3f4b29b05c9d31de7e56b40dd0183c2edd3
verliere <verliere1999@gmail.com>
2024-12-23 18:51:54 +0400

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,6 @@
+name = input('Как вас зовут? ')
+year = 2024
+date = int(input('В каком году вы окончили школу? '))
+kogda = year - date
+print('Добрый день,', name)
+print(name, ', вы окончили школу ', kogda , 'лет назад')
